874a6ce7854e91a813b80a08fac643d55848acf1
Beatrice2403 <s323501@studenti.polito.it>
2026-03-29 14:28:11 +0000

    Consegna esercitazione 3

---
+++ esercitazione_3/build/CMakeFiles/esercitazione_3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/esercitazione_3.dir/link.d"
+  "CMakeFiles/esercitazione_3.dir/main.cpp.o"
+  "CMakeFiles/esercitazione_3.dir/main.cpp.o.d"
+  "esercitazione_3"
+  "esercitazione_3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/esercitazione_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
